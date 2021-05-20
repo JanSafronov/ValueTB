482b2fa4cee1bd832d93bce8e9240feadda6338c
Pomidorka1234 <Jansafronov@gmail.com>
2021-05-20 08:29:11 +0300

    Commit Main tab item

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,18 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Windows.Shell;
+using System.Windows.Resources;
+using System.Windows.Markup;
+using System.Windows.Interop;
+using System.Windows.Diagnostics;
+using System.Windows.Converters;
+using System.Windows.Automation;
+using Microsoft.Windows.Input;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace ValueTB
 {
