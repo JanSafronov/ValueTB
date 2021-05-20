bd7df81f4bbd27d4af81704496e0082defdd4f90
Pomidorka1234 <Jansafronov@gmail.com>
2021-05-20 13:55:08 +0300

    Commit assembly update

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,4 +1,6 @@
 using System.Windows;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 [assembly:ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
@@ -8,3 +10,22 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
+
+/// Assembly Product Information
+[assembly: AssemblyTitle("WPF table managment application")]
+[assembly: AssemblyDescription("WPF Application equipped with table management and calculation of values and transformation of data and import of various table formats")]
+[assembly: AssemblyConfiguration("debug")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WPF Application")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("ValueTB™")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyMetadata("Assembly", "build")]
+
+/// Assembly Universally Unique Identifier
+[assembly: Guid("4f6ca052-6c0d-4d7b-a712-6e5dc4598779")]
+
+/// Assembly Version
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("Start up version of the product")]
