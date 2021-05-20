5d6eca9260850e7a234fc7cb538069a0e60c99db
Pomidorka1234 <Jansafronov@gmail.com>
2021-05-20 14:19:32 +0300

    Commit fix

---
+++ AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+                                     //(used if a resource is not found in the page,
+                                     // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+                                              //(used if a resource is not found in the page,
+                                              // app, or any theme specific resource dictionaries)
+)]
+
+/// Assembly Product Information
+[assembly: AssemblyDescription("WPF Application equipped with table management and calculation of values and transformation of data and import of various table formats")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("ValueTB™")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyMetadata("Assembly", "build")]
+
+/// Assembly Universally Unique Identifier
+[assembly: Guid("4f6ca052-6c0d-4d7b-a712-6e5dc4598779")]
